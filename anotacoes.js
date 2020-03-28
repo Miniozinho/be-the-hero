831ce4ee5e0dfc13901d0a8e42d368fb83f50d6d
gabriel <miniozinho@gmail.com>
2020-03-28 12:55:25 -0300

    back-end da aplicacao

---
+++ anotacoes.js
@@ -0,0 +1,24 @@
+
+/**
+ * Metodos HTTP
+ * 
+ * GET: buscar/listar uma informacao no back-end
+ * POST: Criar uma informacao no back-end
+ * PUT: Alterar uma informacao no back-end
+ * DELETE: Deletar uma informacao no backend
+ */
+
+ /**
+  * Tipos de parametros:
+  * 
+  * Query: Parametros nomeados enviados na rota apos "?" (Filtros, paginacao) localhost/users?nome=Gabriel&idade=17 (const params = request.query;)
+  * Route Params: Parametros utilizqados para indentificar recursos localhosto/users/:id (const params = request.params;)
+  * Request Body: Corpo da requisicao, utilizado para criar ou alterar recursos (const params = request.query;)
+  */
+
+  /**
+   * Banco de dados
+   * 
+   * SQL: MySQL, PostgreSQL, SQLite
+   * NoSQL: MongoDB
+   */
